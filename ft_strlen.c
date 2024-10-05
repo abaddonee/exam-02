5cd91613592b2e2bd8ce5a43b055f500348a7f55
Vera Vucmanovitch <veravucmanovitch@MacBook-Pro-de-Vera.local>
2024-10-05 23:29:14 +0200

    tHssI

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,51 +1,16 @@
-#include  <stdlib.h>
-#include <stdio.h>
-
-char *ft_itoa(int nbr)
+typedef struct    s_list
 {
-    if(nbr == -2147483648) // verification du max int
-		return("-2147483648\0");
-    int len = 0;
-    int temp = nbr;
-    if (temp < 0) // verification si nb est negatif
-    {
-        len++;
-    }
+    struct s_list *next;
+    void          *data;
+}                 t_list;
+
+void    ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
     
-    while(temp) //compte la longeure de la chaine de caractere
+    while(begin_list) // tant que begin list existe 
     {
-        temp /= 10;
-        len++;
+        (*f)(begin_list->data); // applique la fonctionf sur la donne dans begin_list
+        begin_list = begin_list->next; // passe a l'element suivant
     }
-    char *str = (char *)malloc(sizeof(char) * (len + 1)); 
-    if (!str)
-        return (NULL);
 
-    str[len] = '\0'; //ajout du caractere de fin de chaine
-    if(nbr == 0) // si le nombre est 0
-    {
-        str[0] = '0';
-        return (str);
-    }
-    if (nbr < 0) // mettre le signe - si le nombre est negatif
-    {
-        str[0] = '-';
-        nbr = -nbr;
-    }
-    // core de la fonction
-    while(nbr) // ajout des chiffres dans la chaine de caractere
-    {
-        len--;
-        str[len] = (nbr % 10) + '0';
-        nbr /= 10;
-    }
-    return (str);
-}
-
-
-int main()
-{
-    int i = 679;
-    printf("%s\n", ft_itoa(i));
-    return 0;
 }
