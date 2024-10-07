9941159516e46b858836e3ae03b6031758fc433b
abaddonee <160732303+abaddonee@users.noreply.github.com>
2024-10-07 19:04:34 +0200

    Update ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -15,13 +15,15 @@ void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 	{
 		*begin_list = cur->next; // on decale le pointeur de debut de la chaine
 		free(cur); // on supprime l'element
-		ft_list_remove_if(begin_list, data_ref, cmp); // on relance la fonction pour verifier si le prochain element est a supprimer
+		ft_list_remove_if(begin_list, data_ref, cmp); // on relance la fonction pour verifier
+								//si le prochain element est a supprimer
 	}
     // else  
 	if(cmp(cur->data, data_ref) != 0) // si le premier element n'est pas a supprimer
 	{
 		cur = *begin_list; // on reinitialise le pointeur sur le debut de la chaine
-		ft_list_remove_if(&cur->next, data_ref, cmp); // on relance la fonction pour verifier si le prochain element est a supprimer
+		ft_list_remove_if(&cur->next, data_ref, cmp); // on relance la fonction pour verifier 
+							     // si le prochain element est a supprimer
 	}
 }
 
