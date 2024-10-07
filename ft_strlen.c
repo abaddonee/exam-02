186f1384c78836d577675d1fce16128705c7f158
Vera Vucmanovitch <veravucmanovitch@MacBook-Pro-de-Vera.local>
2024-10-07 19:43:03 +0200

    par pietiti

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,29 +1,50 @@
-typedef struct      s_list
+#include <unistd.h>
+#include <stdio.h>
+
+char *ft_strncpy(char *s1, char *s2, int n)
 {
-    struct s_list   *next;
-    void            *data;
-}                   t_list;
+	int i = -1; // on initialise i à -1 pour pouvoir l'incrémenter avant de l'utiliser
 
-void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
-{
-	if (begin_list == NULL || *begin_list == NULL) //si la chaine est vide/finie
-		return;
-
-	t_list *cur = *begin_list; // pointeure sur le debut de la chaine pour parcourir la liste
-
-	if (cmp(cur->data, data_ref) == 0) // si le premier element est a supprimer
-	{
-		*begin_list = cur->next; // on decale le pointeur de debut de la chaine
-		free(cur); // on supprime l'element
-		ft_list_remove_if(begin_list, data_ref, cmp); // on relance la fonction pour verifier
-								//si le prochain element est a supprimer
-	}
-    // else  
-	if(cmp(cur->data, data_ref) != 0) // si le premier element n'est pas a supprimer
-	{
-		cur = *begin_list; // on reinitialise le pointeur sur le debut de la chaine
-		ft_list_remove_if(&cur->next, data_ref, cmp); // on relance la fonction pour verifier 
-							     // si le prochain element est a supprimer
-	}
+	while (++i < n && s2[i]) // on copie les n premiers caractères de s2 dans s1
+		s1[i] = s2[i];
+	s1[i] = '\0'; // on termine la chaine
+	return (s1);
 }
 
+char	**ft_split(char *str)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int nb_words = 0;
+	
+    // on compte le nombre de mots
+	while (str[i])
+	{
+		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
+			i++;
+		if (str[i]) // si on a pas atteint la fin de la chaine
+			nb_words++;
+		while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
+			i++;
+	}
+	
+	char **result = (char **)malloc(sizeof(char *) * (wc + 1)); // on alloue le tableau de pointeurs
+	i = 0;
+	
+	while (str[i])
+	{
+		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')) // on saute les espaces
+			i++;
+		j = i; // on sauvegarde la position du début du mot
+		while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
+			i++;// on avance jusqu'à la fin du mot
+		if (i > j) // si on a un mot
+		{
+			result[k] = (char *)malloc(sizeof(char) * ((i - j) + 1)); // on alloue la mémoire pour le mot
+			ft_strncpy(result[k++], &str[j], i - j); // on copie le mot
+		}
+	}
+	result[k] = NULL; // on termine le tableau de pointeurs
+	return (result);
+}
