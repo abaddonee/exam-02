56ac32238c3fa6e99c1624de7023ead397395f70
Vera Vucmanovitch <veravucmanovitch@MacBook-Pro-de-Vera.local>
2024-10-13 06:02:53 +0200

    sfhjdads

--- ft_strlen.c
+++ ft_strlen.c
@@ -2,58 +2,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char to_lower(char c) 
+int *ft_range(int start, int end)
 {
-    if (c >= 'A' && c <= 'Z')
-    {
-        c += 32;
-    }
-    return c;
-}
-
-
-
-
-int ft_atoi_base(const char *str, int str_base)
-{
-	int result = 0;
-	int sign = 1;
 	int i = 0;
-
-	if (str == NULL || str_base < 2 || str_base > 16) //base 2 a 16
-		return 0;
-
-	if (str[i] == '-') // verifications de signe
+	int len = abs((end - start)) + 1; // abs elle retourne la valeur absolue d'un nombre (en positif)
+                                // + 1 pour inclure le dernier nombre
+	int *result = (int *)malloc(sizeof(int) * len);
+	
+	while (i < len) // tant que i est inférieur à la longueur
 	{
-		sign = -1;
-		i++;
+		if (start < end) // si le début est inférieur à la fin
+		{
+			result[i] = start; // on stocke le début dans le tableau
+			start++; // on incrémente le début
+			i++; // on incrémente i
+		}
+		else // sinon
+		{
+			result[i] = start; // on stocke le début dans le tableau
+			start--; // on décrémente le début
+			i++; // on incrémente i
+		}
 	}
-
-	while (str[i]) // boucle pour parcourir la chaine
-	{
-		if (str[i] >= '0' && str[i] <= '9') // si c'est un chiffre
-            result = result * str_base + str[i] - '0'; // on multiplie le resultat par la base et on ajoute le chiffre
-        else if (to_lower(str[i]) >= 'a' && to_lower(str[i]) <= 'f') // si c'est une lettre
-            result = result * str_base + to_lower(str[i]) - 'a' + 10;   // on multiplie le resultat par la base et on ajoute la lettre
-        else // si c'est un caractere non valide
-            break; // on sort de la boucle
-        i++; // on incremente i
-	}
-	return (result * sign); // on retourne le resultat avec le signe
+        return (result);
 }
 
-
-
-int main(void)
+int *ft_range(int start, int end)
 {
-    const char *str = "-C";
-    int str_base = 16;
-    int result = ft_atoi_base(str, str_base);
+	int i = 0;
     
-    printf("Result: %d\n", result);
-    
-    return 0;
+	int len = abs((end - start)) + 1; // abs elle retourne la valeur absolue d'un nombre (en positif)
+                                // + 1 pour inclure le dernier nombre
+	int *result = (int *)malloc(sizeof(int) * len);
+	
+	while (i < len) // tant que i est inférieur à la longueur
+	{
+		if (start < end) // si le début est inférieur à la fin
+		{
+			result[i] = start; // on stocke le début dans le tableau
+			start++; // on incrémente le début
+			i++; // on incrémente i
+		}
+		else // sinon
+		{
+			result[i] = start; // on stocke le début dans le tableau
+			start--; // on décrémente le début
+			i++; // on incrémente i
+		}
+	}
+        return (result);
 }
 
 
+int main()
+{
+  
+    int *result = ft_range(6, 3);
+    
+    int i = 0;
+    
+    while (result[i] != '\0')
+    {
+        printf("%d\n", result[i]);
+        i++;
+    }
+    return (0);
+}
 
