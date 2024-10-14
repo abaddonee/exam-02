81b5886c0e5d832eba98f00089204eca2c053051
abaddonee <160732303+abaddonee@users.noreply.github.com>
2024-10-14 08:37:29 +0200

    Update ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -13,19 +13,22 @@ int main(int argc, char **argv)
         
         while(str1[i] != '\0') // tant que str1 n'est pas fini
         {
-            while (str2[j] != '\0' && str2[j] != str1[i]) // tant que str2 n'est pas fini et que str2 n'est pas égale à str1[i]
+            while (str2[j] != '\0' && str2[j] != str1[i]) // tant que str2 n'est pas fini et que str2 
+                                                            //   n'est pas égale à str1[i]
             {
                 j++;
             }
              if (str2[j] == '\0') // si str2 est fini
             {
-                write(1, "0\n", 2); // on affiche 0 car str1 n'est pas fini est str2 est fini donc str1 n'est pas dans str2
+                write(1, "0\n", 2); // on affiche 0 car str1 n'est pas fini est str2 est fini 
+                                    //donc str1 n'est pas dans str2
                 return 0; 
             }
             j++; // on incrémente j pour passer à la lettre suivante
             i++; // on incrémente i pour passer à la lettre suivante
         }
-        write(1, "1\n", 2); // si on arrive ici c'est que str1 est fini et qu'on a trouvé toutes les lettres de str1 dans str2
+        write(1, "1\n", 2); // si on arrive ici c'est que str1 est fini et 
+                            //qu'on a trouvé toutes les lettres de str1 dans str2
         return 0;
         
     }
