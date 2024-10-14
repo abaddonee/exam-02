318a6532221df60eca75be7261ece5f37cc40197
Vera Vucmanovitch <veravucmanovitch@MacBook-Pro-de-Vera.local>
2024-10-14 09:16:34 +0200

    testt

--- ft_strlen.c
+++ ft_strlen.c
@@ -2,36 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
+unsigned int    lcm(unsigned int a, unsigned int b)
 {
-    if(argc == 3)
-    {
-        int i = 0; // index pour str1
-        int j = 0; // index pour str2
-        char *str1 = argv[1]; 
-        char *str2 = argv[2];
-        
-        while(str1[i] != '\0') // tant que str1 n'est pas fini
-        {
-            while (str2[j] != '\0' && str2[j] != str1[i]) // tant que str2 n'est pas fini et que str2 
-                                                            //   n'est pas égale à str1[i]
-            {
-                j++;
-            }
-             if (str2[j] == '\0') // si str2 est fini
-            {
-                write(1, "0\n", 2); // on affiche 0 car str1 n'est pas fini est str2 est fini 
-                                    //donc str1 n'est pas dans str2
-                return 0; 
-            }
-            j++; // on incrémente j pour passer à la lettre suivante
-            i++; // on incrémente i pour passer à la lettre suivante
-        }
-        write(1, "1\n", 2); // si on arrive ici c'est que str1 est fini et 
-                            //qu'on a trouvé toutes les lettres de str1 dans str2
-        return 0;
-        
+    unsigned int nbr;
+    if (a == 0 || b == 0) // si a ou b est égal à 0, on retourne 0
+        return (0);
+    if(a > b) // on prend le plus grand nombre
+        nbr = a;
+    else 
+        nbr = b;
+    while(1) // on boucle jusqu'à trouver le plus petit multiple commun
+    { // sa sera le plus petit car le if affiche le premier trouvé
+        if (nbr % a == 0 && nbr % b == 0) // si nbr est un multiple de a et b, on retourne nbr
+            return (nbr);
+        nbr++; // on incrémente nbr
     }
-    write(1, "0\n", 2); // si on arrive ici c'est que le nombre d'arguments n'est pas bon
-    return 0;
+}
+
+int main(int ac, char **av)
+{
+    int a = 4;
+    int b = 6;
+    printf("%d", lcm(a, b));
+    write(1, "\n", 1);
+    return (0);
 }
