04c6adfa801c0aea75512e40710e04d589675014
Vera Vucmanovitch <veravucmanovitch@MacBook-Pro-de-Vera.local>
2024-10-14 10:13:59 +0200

    gfysudd

--- ft_strlen.c
+++ ft_strlen.c
@@ -8,25 +8,25 @@ int main (int argc, char **argv)
 {
     if (argc == 3)
     {
-        int nbr1 = atoi(argv[1]);
+        int nbr1 = atoi(argv[1]); 
         int nbr2 = atoi(argv[2]);
         if(nbr1 == 0 || nbr2 == 0)
         {
-            printf("\n");
+            printf("\n"); // si un des deux nombre est égale à 0, on ne peut pas faire de pgcd
             return 0;
         }
-        while(nbr1 != nbr2)
+        while(nbr1 != nbr2) // tant que les deux nombres sont différents
         {
-            if(nbr1 > nbr2)
+            if(nbr1 > nbr2) // on soustrait le plus grand au plus petit
             {
-                nbr1 -= nbr2;
+                nbr1 -= nbr2; // on soustrait parce que le pgcd est le reste de la division euclidienne
             }
-            else
+            else 
             {
                 nbr2 -= nbr1;
             }
         }
-        printf("%d", nbr1);
+        printf("%d", nbr1); // on affiche le pgcd car les deux nombres sont égaux
         
     }
     printf("\n");
