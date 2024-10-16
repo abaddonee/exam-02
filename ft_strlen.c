23eaf6dfe0b3a1a9885bc1be3a15e456033dac89
Vera Vucmanovitch <veravucmanovitch@MacBook-Pro-de-Vera.local>
2024-10-16 20:48:24 +0200

    gfysuddsss

--- ft_strlen.c
+++ ft_strlen.c
@@ -3,52 +3,26 @@
 #include <stdlib.h>
 
 
-void ft_write_nb(int nb)
+int main(int argc, char **argv)
 {
-    char base[] = "0123456789";
-    if(nb >= 10)
+    if (argc == 2)
     {
-        ft_write_nb(nb / 10);
-        ft_write_nb(nb % 10);
-    }
-    else
-    {
-        write(1, &base[nb], 1);
-    }
-}
-
-int ft_atoi(char *str)
-{
-    int resuly = 0;
-    while(*str)
-    {
-        resuly = resuly * 10 + *str - '0';
-        str++;
-    }
-    return resuly;
-}
-
-int main(int ac, char **av)
-{
-    if(ac == 2)
-    {
-        int i = 1;
-        int nbr = ft_atoi(av[1]); 
-        while(i <= 9)
+        int i = 0;
+        while(argv[1][i] != '\0')
         {
-            ft_write_nb(i);
-            write(1, " x ", 3);
-            ft_write_nb(nbr);
-            write(1, " = ", 3);
-            ft_write_nb(i * nbr);
-            write(1, "\n", 1);
+            if(argv[1][i] >= 'a' && argv[1][i] <= 'z') // si c'est une minuscule
+            {
+                argv[1][i] = ('a' + 'z') - argv[1][i]; // on inverse la lettre
+            }
+            else if(argv[1][i] >= 'A' && argv[1][i] <= 'Z') // si c'est une majuscule
+            {
+                argv[1][i] = ('A' + 'Z')  - argv[1][i]; // on inverse la lettre 
+            }
+            
             i++;
         }
+        write(1, argv[1], i);
     }
-    else
-    {
-        write(1, "\n", 1);
-    }
+    write(1, "\n", 1);
     return 0;
-    
 }
