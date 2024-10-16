1dd29259ea4ca42c8eeade2069ddc72f0dbecefc
Vera Vucmanovitch <veravucmanovitch@MacBook-Pro-de-Vera.local>
2024-10-16 21:24:47 +0200

    gfysuddsss

--- ft_strlen.c
+++ ft_strlen.c
@@ -3,29 +3,35 @@
 #include <stdlib.h>
 
 
+
 int main(int argc, char **argv)
 {
     if (argc == 2)
     {
         int i = 0;
-        while(argv[1][i] != '\0')
-        {
-            if(argv[1][i] >= 'A' && argv[1][i] <= 'Z') //si c'est une majuscule
-            {
-                write(1, "_", 1); //on ecrit un underscore
-                argv[1][i] += 32; //on ajoute 32 pour passer en minuscule
-                write(1, &argv[1][i], 1); //on ecrit la lettre
-                
-                
-            }
-            else
-            {
-                write(1, &argv[1][i], 1);
-            }
+
+        // Parcourir jusqu'à la fin de la chaîne
+        while (argv[1][i] != '\0')
             i++;
+
+        // Remonter pour ignorer les espaces à la fin (si présents)
+        while (i > 0 && (argv[1][i - 1] == ' ' || argv[1][i - 1] == '\t' || argv[1][i - 1] == '\n'))
+            i--;
+
+        // Trouver le début du dernier mot
+        int j = i;
+        while (j > 0 && argv[1][j - 1] != ' ' && argv[1][j - 1] != '\t' && argv[1][j - 1] != '\n')
+            j--;
+
+        // Afficher le dernier mot
+        while (j < i)
+        {
+            write(1, &argv[1][j], 1);
+            j++;
         }
-        
     }
+
+    // Saut de ligne à la fin
     write(1, "\n", 1);
     return 0;
 }
