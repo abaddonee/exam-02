2474da4f75b50e0601869326267cc7bfaedb9108
Vera Vucmanovitch <veravucmanovitch@MacBook-Pro-de-Vera.local>
2024-10-17 03:22:48 +0200

    gfysuddsss

--- ft_strlen.c
+++ ft_strlen.c
@@ -7,35 +7,41 @@
 
 
 
-int main(int argc, char **argv)
+char *ft_strrev(char *str)
 {
-    if (argc == 3)
-    {
-        int i = 0;  // Index pour argv[1] la chaine a trouver dans argv[2]
-        int j = 0;  // Index pour argv[2] 
+    int i = 0;
+    int j = 0;
+    char temp;
 
-        // Parcourir argv[2] pour chercher les caractères de argv[1] dans l'ordre
-        while (argv[2][j] != '\0')
-        {
-            if (argv[1][i] == argv[2][j])
-            {
-                i++;  // Avancer dans argv[1] si un caractère est trouvé
-            }
-            if (argv[1][i] == '\0')
-            {
-                // Si tous les caractères de argv[1] ont été trouvés dans argv[2]
-                write(1, argv[1], i);
-                write(1, "\n", 1);
-                return 0;
-            }
-            j++;  // Avancer dans argv[2]
-        }
+    // Trouver la longueur de la chaîne
+    while (str[i] != '\0')
+        i++;
+    
+    // Réduire i pour pointer sur le dernier caractère
+    i--;
+
+    // Inverser les caractères
+    while (j < i)
+    {
+        // Échanger les caractères
+        temp = str[j];
+        str[j] = str[i];
+        str[i] = temp;
+
+        // Avancer j et reculer i
+        j++;
+        i--;
     }
-    // Si argv[1] n'est pas trouvé dans argv[2] ou si les arguments sont incorrects
-    write(1, "\n", 1);
+
+    return (str);
+}
+
+
+int main()
+{
+    char str[] = "Hello World!";
+    printf("%s\n", ft_strrev(str));
     return 0;
 }
    
  
-
-
