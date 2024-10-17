83218e8fddcbe217f6e28741005b1d60bc3066d2
Vera Vucmanovitch <veravucmanovitch@MacBook-Pro-de-Vera.local>
2024-10-17 04:04:16 +0200

    gfysuddsss

--- ft_strlen.c
+++ ft_strlen.c
@@ -3,31 +3,71 @@
 #include <stdlib.h>
 #include <string.h>
 
-unsigned char reverse_bits(unsigned char octet)
+int main(int ac, char **av)
 {
-    unsigned char result;
-    unsigned char i;
-
-    i = 0;
-    result = 0;
-    while (i < 8) // 8 bits
+    if (ac == 3)
     {
-        result = result << 1; //on décale le bit de 1 vers la gauche
-        result = result | (octet & 1); //on ajoute le bit de poids faible de octet
-        octet = octet >> 1; //on décale octet de 1 vers la droite
-        i++; //on incrémente i
+        unsigned char seen[256] = {0}; // 256 c'est le nombre de caractères possibles
+        int i = 0; 
+        int j = 0;
+
+        while (av[1][i]) // tant que la chaine de caractère n'est pas finie
+        {
+            if (!seen[(unsigned char)av[1][i]]) // si le caractère n'a pas été vu
+            {
+                write(1, &av[1][i], 1); // on l'affiche
+                seen[(unsigned char)av[1][i]] = 1; // on le marque comme vu
+            }
+            i++;
+        }
+        while (av[2][j]) // tant que la chaine de caractère n'est pas finie
+        {
+            if (!seen[(unsigned char)av[2][j]])
+            {
+                write(1, &av[2][j], 1);
+                seen[(unsigned char)av[2][j]] = 1;
+            }
+            j++;
+        }
     }
-    return (result); //on retourne le résultat
+    write(1, "\n", 1);
+    return 0;
 }
 
-int main()
+
+int main(int ac, char **av)
 {
-    unsigned char byte;
-
-    byte = 2;
-    print_bits(byte);
-    return (0);
+    int i;
+    int j;
+    
+    if (ac == 3)
+    {
+        j = 0;
+        i = 0;
+        while (av[2][j]) // tant que la chaine de caractère n'est pas finie
+        {
+            if (av[1][i] == av[2][j]) // si le caractère est le même
+                i++; // on passe au caractère suivant
+            j++;
+        }
+        if (av[1][i] == '\0') // si on est arrivé à la fin de la chaine
+        {
+            i = 0;
+            while(av[1][i]) // tant que la chaine de caractère n'est pas finie
+            {
+                write(1, &av[1][i], 1);
+                i++;
+            }
+        }
+    }
+    write(1, "\n", 1);
+    return(0);
 }
+
+
+
+
+
 
 
 
